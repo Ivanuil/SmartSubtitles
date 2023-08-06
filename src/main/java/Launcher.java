7a662430310a357e0383553e58f1cb840125b826
Ivanuil <ivo-pronin@yandex.ru>
2023-08-06 20:17:07 +0300

    Added file choosing

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,13 +1,28 @@
 import me.bush.translator.Language;
 
+import javax.swing.*;
 import java.io.FileNotFoundException;
 
 public class Launcher {
     public static void main(String[] args) throws FileNotFoundException {
         PlayerForm player = new PlayerForm();
         player.setVisible(true);
-        player.playMedia("C:\\Users\\ivo-p\\dev\\SmartSubtitles\\src\\main\\java\\video.mkv");
-        player.playSubtitles("C:\\Users\\ivo-p\\dev\\SmartSubtitles\\src\\main\\java\\subtitles.srt");
+
+        JFileChooser videoFileChooser = new JFileChooser();
+        int responseCodeVideo = videoFileChooser.showOpenDialog(player);
+        JFileChooser subsFileChooser = new JFileChooser();
+        int responseCodeSubs = subsFileChooser.showOpenDialog(player);
+
+        if (responseCodeVideo == JFileChooser.APPROVE_OPTION)
+            player.playMedia(videoFileChooser.getSelectedFile().getPath());
+        else
+            player.playMedia("C:\\Users\\ivo-p\\dev\\SmartSubtitles\\src\\main\\java\\video.mkv");
+
+        if (responseCodeSubs == JFileChooser.APPROVE_OPTION)
+            player.playSubtitles(subsFileChooser.getSelectedFile().getPath());
+        else
+            player.playSubtitles("C:\\Users\\ivo-p\\dev\\SmartSubtitles\\src\\main\\java\\subtitles.srt");
+
         player.addTranslator(new SubtitlesTranslator(Language.AUTO, Language.RUSSIAN));
     }
 
